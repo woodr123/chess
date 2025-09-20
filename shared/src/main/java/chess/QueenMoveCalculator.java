0b11d66e53c8785b6843b2cff4da51362a2b03ff
Sabrina Davis <woodr123@byu.edu>
2025-09-19 22:02:18 -0600

    completed QueenMoveCalculator and passed all tests, easiest becuase I could just copy from Rook and Bishop.

--- shared/src/main/java/chess/QueenMoveCalculator.java
+++ shared/src/main/java/chess/QueenMoveCalculator.java
@@ -7,6 +7,41 @@ import java.util.List;
 public class QueenMoveCalculator implements PieceMovesCalculator {
 
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return null;
+        List<ChessMove> moves = new ArrayList<>();
+        int currentRow = myPosition.getRow();
+        int currentColumn = myPosition.getColumn();
+
+        int[][] directions = {
+                {1, 0}, {0, 1},
+                {-1, 0}, {0, -1},
+                {1, -1}, {1, 1},
+                {-1, -1}, {-1, 1}
+        };
+
+        for (int i = 0; i < 8; i++) {
+            int[] row_colDirection = directions[i];
+            currentRow = myPosition.getRow();
+            currentColumn = myPosition.getColumn();
+            while (true) {
+                currentRow += row_colDirection[0];
+                currentColumn += row_colDirection[1];
+                if (currentRow < 1 || currentRow > 8 || currentColumn < 1 || currentColumn > 8) {
+                    break;
+                }
+                ChessPosition newPosition = new ChessPosition(currentRow, currentColumn);
+                if (board.getPiece(newPosition) == null) {
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                }
+                else if (board.getPiece(newPosition).getTeamColor() == board.getPiece(myPosition).getTeamColor()) {
+                    break;
+                }
+                else if ((board.getPiece(newPosition).getTeamColor() != board.getPiece(myPosition).getTeamColor())) {
+                    moves.add(new ChessMove(myPosition, newPosition,null ));
+                    break;
+                }
+            }
+        }
+
+        return moves;
     }
 }
